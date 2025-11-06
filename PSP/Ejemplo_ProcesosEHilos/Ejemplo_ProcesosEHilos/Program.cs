08676f9b3f1b292a91eeb568dfee1c3f508847d5
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-11-06 13:58:22 +0100

    Ejercicios de Procesos

---
+++ PSP/Ejemplo_ProcesosEHilos/Ejemplo_ProcesosEHilos/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using System.Diagnostics;
+
+namespace Ejemplo_ProcesosEHilos
+{
+    internal class Program
+    {
+
+        class ProcesosGuapardos
+        {
+            //Process paint = Process.Start("mspaint.exe");
+            public void EjemploProcesos0()
+            {
+                Console.WriteLine("Hola, que programa quieres abrir??");
+
+                bool input_valido = false;
+                while (!input_valido)
+                {
+
+                    Console.WriteLine("1. Calculadora");
+                    Console.WriteLine("2. Explorador de carpetas de Windows");
+                    Console.WriteLine("3. Paint");
+
+
+
+                    // le ponemos un true para que no se vea la tecla pulsada
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.Key == ConsoleKey.NumPad1 || key.Key == ConsoleKey.D1)
+                    {
+
+
+                        Console.WriteLine("Lanzando calculadora");
+
+                        Process calculadora = Process.Start("calc.exe");
+                    }
+                    else if (key.Key == ConsoleKey.NumPad2 || key.Key == ConsoleKey.D2)
+                    {
+                        Console.WriteLine("Lanzando el explorador de carpetas");
+                        Process calculadora = Process.Start("explorer.exe");
+
+                    }
+                    else if (key.Key == ConsoleKey.NumPad3 || key.Key == ConsoleKey.D3)
+                    {
+
+                        Console.WriteLine("Lanzando MSPaint");
+                        Process paint = Process.Start("mspaint.exe");
+
+                        // esperate a que el proceso termine, vamos que se cierre el paint
+                        // los inputs se guardan, tendriamos que hacer una limpieza de buffer
+
+                        paint.WaitForExit();
+
+                        while (Console.KeyAvailable)
+                        {
+                            Console.ReadKey(true);
+                        }
+
+                        Console.WriteLine("Cerrando MSPaint...");
+
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Se pulso otra tecla");
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            ProcesosGuapardos p = new ProcesosGuapardos();
+            //p.EjemploProcesos0();
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Uso: Subproceso_SumaParcial <inicio> <fin>");
+                return;
+            }
+
+            int inicio = int.Parse(args[0]);
+            int fin = int.Parse(args[1]);
+            long suma = 0;
+            long numPrimos = 0;
+
+            for (int i = inicio; i <= fin; i++)
+            {
+                suma += i;
+                if (EsPrimo(i))
+                {
+                    numPrimos++;
+                }
+
+            }
+
+            Console.WriteLine(numPrimos);
+
+            bool EsPrimo(int numero)
+            {
+                if (numero < 2) return false;
+                if (numero == 2) return true;
+                if (numero % 2 == 0) return false;
+
+                for (int i = 3; i <= Math.Sqrt(numero); i += 2)
+                {
+                    if (numero % i == 0)
+                        return false;
+                }
+                return true;
+            }
+        }
+    }
+}
---
+++ PSP/Ejemplo_ProcesosEHilos/SumaMultiproceso/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaMultiproceso
+{
+
+    internal class Program
+    {
+
+        // sacamos numeros primos de los 4 rangos y la suma total de cuantos ha habido ellos
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Suma multiproceso ===");
+
+            int[,] rangos = new int[4, 2] { { 1, 1000 }, { 1001, 2000 }, { 2001, 3000 }, { 3001, 4000 } };
+            long sumaTotal = 0;
+            
+            
+
+            for (int i = 0; i < rangos.GetLength(0); i++)
+            {
+                int inicio = rangos[i, 0];
+                int fin = rangos[i, 1];
+
+                Process proceso = new Process();
+                //"C:\Users\CAMPUSFP\source\repos\EjemploDebugging\EjemploDebugging\bin\Debug\EjemploDebugging.exe"
+                //C:\Users\CAMPUSFP\source\repos\EjemploDebugging\EjemploDebugging\bin\Debug\EjemploDebugging.exe
+                proceso.StartInfo.FileName = @"C:\programacion\java\PSP\Ejemplo_ProcesosEHilos\Ejemplo_ProcesosEHilos\bin\Debug\Ejemplo_ProcesosEHilos.exe";
+                proceso.StartInfo.Arguments = inicio + " " + fin;
+                proceso.StartInfo.RedirectStandardOutput = true;
+                proceso.StartInfo.UseShellExecute = false;
+                proceso.Start();
+                
+
+                string salida = proceso.StandardOutput.ReadToEnd();
+                proceso.WaitForExit();
+                
+                long parcial = long.Parse(salida);
+                
+
+                Console.WriteLine("Rango " + inicio + "-" + fin + " = " + parcial);
+                sumaTotal += parcial;
+
+
+            }
+
+            Console.WriteLine("\nPrimos totales = " + sumaTotal);
+        }
+    }
+}
