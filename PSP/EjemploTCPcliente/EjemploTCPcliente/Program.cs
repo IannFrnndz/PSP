db60834a35b1c5d99c2685f60ae1cf62a73c0d75
Ian Fernández Gamo <ian.fernandezgamo24@campusfp.es>
2025-12-12 08:57:04 +0100

    Enviar mensajes y recibirlos

---
+++ PSP/EjemploTCPcliente/EjemploTCPcliente/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace EjemploTCPcliente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // creamos un cliente TCP sin conectarme aun
+            TcpClient clientardo = new TcpClient();
+
+            // intento conectar al server que este en la ip(localhost , es decir, yo mismo )
+            //se bloquea hasta que se conecte o falle
+            clientardo.Connect("127.0.0.1",5000);
+
+            // obteno el flujo de datos para recibir datos
+            NetworkStream flujillo = clientardo.GetStream();
+
+            
+            byte[] yoQuieroBuffer = new byte[1024];
+
+            // read se bloquea hasta que el servidor envie datos
+            // devuelve la cantidad de bytes leidos/ recibidos
+            int cosasLeidas = flujillo.Read(yoQuieroBuffer, 0, yoQuieroBuffer.Length);
+
+            string mensajardo;
+
+            mensajardo = Encoding.UTF8.GetString(yoQuieroBuffer, 0, cosasLeidas);
+
+            Console.WriteLine("El server te dice esto macho: " + mensajardo);
+
+
+        }
+    }
+}
---
+++ PSP/EjemplosTCPserver/EjemplosTCPserver/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace EjemplosTCPserver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //servidor que escucha en cualquier IP en el puerto local
+
+            TcpListener miServerGuapo = new TcpListener(IPAddress.Any, 5000);
+            // abre el puerto y empieza a escuchar conexiones entrantes
+            miServerGuapo.Start();
+            Console.WriteLine("Esperando a que se conecte mi pana...");
+
+
+            // acepta una conexion entrante
+            //AcceptTcpClient es una llamada bloqueante
+            // cuando alguien se conecta, devuelve un TcpClient 
+            TcpClient cliente = miServerGuapo.AcceptTcpClient();
+
+            Console.WriteLine("Cliente conectado");
+
+            // Obtener el flujo del datos asociado al socket del el cliente asociado
+            // todo lo que esriba en flujodatos se enviará al cliente
+            NetworkStream flujodatos = cliente.GetStream();
+
+            bool cerrar = false;
+
+             string mensaje = "";
+
+            while (!cerrar)
+            {
+                Console.WriteLine("Escribe un mensaje: ");
+                string mensaje = Console.ReadLine();
+                byte[] puroDataChorizo = Encoding.UTF8.GetBytes($"Mensaje del cliente: {mensaje} ");
+
+                flujodatos.Write(puroDataChorizo, 0, puroDataChorizo.Length);
+
+                if (mensaje.ToLower() == "cerrar")
+                {
+                    cerrar = true;
+                }
+                
+            }
+            ;
+
+
+            // cierra la conexion con el clientardo
+            cliente.Close();
+
+            // cierra el servidor guapo y deja de escuchar
+            miServerGuapo.Stop();
+
+
+        }
+    }
+}
